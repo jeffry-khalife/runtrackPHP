bae803b8f9d1feb17218a47063278f9856b5e1cc
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-01-13 13:54:11 +0100

    Job 01 of day 1 finished

---
+++ jour01/job01/index.php
@@ -0,0 +1,18 @@
+<?php 
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+echo $str2," ",$str," ",$str3,"<br/>";
+
+$val = 6;
+echo "val = ",$val,"<br/>";
+
+$val = 6+4;
+echo "val = ",$val,"<br/>";
+
+$myBool = true;
+echo "myBool = ",$myBool,"<br/>";
+
+$myBool = false;
+echo "myBool = ",$myBool,"<br/>";
+?>
