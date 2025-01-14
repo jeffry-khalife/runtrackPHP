4402bffb480cf2f4b92e9c1a51fdae0427c16815
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-01-14 13:11:23 +0100

    Job 03 of day 1 finished

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -4,11 +4,20 @@ $str = "Salut";
 $valeur = 12;
 $moyenne = 10.9;
 
+
 $tableau = [
     "type"  => array(gettype($bool),gettype($str),gettype($valeur) ,gettype($moyenne)),
     "nom" => array("bool", "str", "valeur", "moyenne"),
     "valeur"   => array("$bool", "$str", "$valeur","$moyenne")
 ];
 
+
+
 print_r ($tableau);
+/*
+print_r ($tableau["type"]);
+print_r ($tableau["nom"]);
+print_r ($tableau["valeur"]);
+*/
+
 ?>
