1ecca9787ec2ddf4cd44fbee960182cff47347ed
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-01-14 13:12:41 +0100

    Job 02 of day 2 finished

---
+++ jour02/job02/index.php
@@ -0,0 +1,22 @@
+<?php 
+for ( $i=0; $i<=1337; $i++)
+{
+    if ($i === 26) {
+        echo " ";
+    }
+    elseif ($i === 37) {
+        echo " ";
+    }
+    elseif ($i === 88) {
+        echo " ";
+    }
+    elseif ($i === 1111) {
+        echo " ";
+    }
+    else {
+    echo $i;
+    echo "<br>";
+    }       
+}
+
+?>
