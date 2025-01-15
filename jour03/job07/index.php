deede87a5ea8f6f513afcf6d2f07c121ff2a3fd6
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-01-15 15:31:44 +0100

    Job 07 of day 3 finished

---
+++ jour03/job07/index.php
@@ -0,0 +1,19 @@
+<?php 
+$str = "Certaines choses changent, et d'autres ne changeront jamais.";
+$inversement = " ";
+$longeur= 0;
+while(isset($str[$longeur]) != null){
+    $longeur++;
+}
+for ($i = 0; $i < $longeur; $i++) {
+    if ($i == $longeur - 1) {
+        $inversement .= $str[0];
+    } 
+    else {
+        $inversement .= $str[$i +1] ;
+    }
+}
+
+echo $inversement;
+
+?>
