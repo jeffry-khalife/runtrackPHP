2d02d91e10f83bb0811e3c17f4c638cde293518e
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-01-16 12:22:09 +0100

    Job 03 of day 4 finished

---
+++ jour04/job03/index.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get</title>
+</head>
+<body>
+<form action="index.php" method="post">
+   <label>Votre nom :</label>
+   <input name="nom" id="nom" type="text" /><br>
+
+   <label>Votre Prenom :</label>
+   <input name="prenom" id="prenom" type="text" /><br>
+
+   <label>Votre âge :</label>
+   <input name="age" id="age" type="number" />
+
+   <button type="submit">Valider</button>
+</form>
+<?php
+$nombreget = 0 ;
+foreach ($_POST as $key => $values) {
+    if ($values) { 
+        $nombreget++;
+    }
+}
+echo "le nombre de post est ",$nombreget;
+
+?>
+</body>
+</html>
