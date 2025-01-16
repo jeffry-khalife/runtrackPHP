43dbaa4e635c189140f97ed08ef994580cf2fd11
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-01-16 11:51:52 +0100

    Job 02 of day 4 finished

---
+++ jour04/job02/index.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get</title>
+</head>
+<body>
+<form action="index.php" method="get">
+   <label>Votre nom :</label>
+   <input name="nom" id="nom" type="text" /><br>
+
+   <label>Votre Prenom :</label>
+   <input name="prenom" id="prenom" type="text" /><br>
+
+   <label>Votre âge :</label>
+   <input name="age" id="age" type="number" />
+
+   <button type="submit">Valider</button>
+</form>
+<?php
+if (isset($_GET)) {
+        echo "<table border='1'>
+            <tr>
+                <td>Nom</td>
+                <td>Prenom</td>
+                <td>Age</td>
+            </tr>
+            <tr>
+                <td>{$_GET['nom']}</td>
+                <td>{$_GET['prenom']}</td>
+                <td>{$_GET['age']}</td>
+            </tr>
+                </table>";
+}
+?>
+</body>
+</html>
